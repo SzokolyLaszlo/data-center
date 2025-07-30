ce853deef26ec2d56a1d8954901b36269abe84ca
Szokoly László <szokoly.laszlo@fokefe.hu>
2025-07-30 16:55:00 +0200

    Added server IP to CORS.

--- backend/config/AllowedOrigins.js
+++ backend/config/AllowedOrigins.js
@@ -2,7 +2,8 @@ const allowedOrigins = [
   "http://localhost:80",
   "http://localhost:3000",
   "http://localhost:5000",
-  "http://localhost:8080"
+  "http://localhost:8080",
+  "185.33.53.51"
 ];
 
 module.exports = allowedOrigins;
